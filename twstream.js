d3bb0e1c3cc684fad38f20ee987c17a70bdaf573
Koji Ota <afterjnih@gmail.com>
2015-06-14 09:57:26 +0900

    initial commit

---
+++ twstream.js
@@ -0,0 +1,78 @@
+'use strict';
+require('dotenv').load();
+var cheerio = require('cheerio-httpcli');
+var url = require('url');
+var twitter = require('twitter');
+var WebSocketServer = require('ws').Server;
+
+var twit = new twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+var inputUrl = process.argv[2];
+console.log('start monitoring: ' + inputUrl);
+
+var fs = require('fs');
+var app = require('http').createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(fs.readFileSync('index.html'));
+}).listen(3000);
+
+var connects = [];
+
+function broadcast (message) {
+    connects.forEach(function (socket, i) {
+        socket.send(message);
+    });
+}
+
+var wss = new WebSocketServer({'server': app});
+wss.on('connection', function (ws) {
+    console.log('WebSocketServer connected');
+    connects.push(ws);
+    broadcast('connected sockets: ' + connects.length);
+    ws.on('message', function (message) {
+        console.log('received -' + message);
+        broadcast(message);
+    });
+
+    ws.on('close', function () {
+        console.log('stopping client send "close"');
+
+        connects = connects.filter(function (conn, i) {
+            return (conn === ws) ? false : true;
+        });
+        broadcast('connected sockets: ' + connects.length);
+    });
+});
+
+cheerio.fetch(inputUrl, function(err, $, res){
+  var twitterScreenNames = [];
+  $('a').each(function(e){
+    if ($('a')[e].attribs.title === 'Twitterを見る'){
+      twitterScreenNames.push(url.parse($('a')[e].attribs.href, true).query.screen_name);
+    }
+  });
+
+  var twitterScreenNamesSet = twitterScreenNames.filter(function(screenName, index, self){
+    return self.indexOf(screenName) === index;
+  });
+
+  var userIds;
+  twit.get('users/lookup', {screen_name: twitterScreenNamesSet.join(',')}, function(error, users, response){
+    userIds = users.reduce(function(previousUsers, currentUser){
+      previousUsers.push(currentUser.id);
+      return previousUsers;
+    }, []);
+    console.log(userIds.length + 'users!');
+    twit.stream('statuses/filter', {follow: userIds.join(',')}, function(stream) {
+      stream.on('data', function (data) {
+        broadcast(data.text);
+        console.log(data);
+      });
+    });
+  });
+});
